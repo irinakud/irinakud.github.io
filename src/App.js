01beb855af0146a0261742e1bbc2ead405f3c77e
irinakud <irbaska@gmail.com>
2021-04-06 15:42:48 +1000

    Added link to another gh page

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          className="Tribute Page"
+          href="https://irinakud.github.io/tribute-page"
+          target="_yop"
           rel="noopener noreferrer"
         >
           Learn React
