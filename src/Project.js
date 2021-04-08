780d509bf76bf284fe06b8efb92c60ef639c264c
irinakud <irbaska@gmail.com>
2021-04-08 14:19:26 +1000

    Changed Tech style and layout

--- src/Project.js
+++ src/Project.js
@@ -7,9 +7,9 @@ class Project extends React.Component {
       <div className={"card filterDiv " + this.props.tech}>
         <a href={this.props.url} target="_top"><img src={this.props.img_url} className="card-img-top" alt={this.props.id + " project snapshot"}/></a>
         <div className="card-body">
-          <h5 className="card-title">{this.props.id}</h5>
-          <p className="card-text tech">({this.props.tech}).</p>
           <Tech tech={this.props.tech}/>
+          <h5 className="card-title">{this.props.id}</h5>
+          {/* <p className="card-text tech">({this.props.tech}).</p> */}
         </div>
       </div>
     );
--- src/Tech.js
+++ src/Tech.js
@@ -10,11 +10,12 @@ class Tech extends React.Component {
       
       return (           
         <div className="card-text tech">
-          { this.props.tech.includes('bootstrap') && <i className="fab fa-bootstrap"></i> }
-          { this.props.tech.includes('react') && <i className="fab fa-react"></i> }
-          { this.props.tech.includes('css') && <i className="fab fa-css3"></i> }
-          { this.props.tech.includes('html') && <i className="fab fa-html5"></i> }
-          { this.props.tech.includes('jquery') && <img style={styles} src='https://raw.githubusercontent.com/HappyMe777/portfolio/master/img/jquery-icon.png' alt=""/> }
+          { this.props.tech.includes('javascript') && <span className="tech-icon"><i className="fab fa-js"></i></span> }
+          { this.props.tech.includes('react') && <span className="tech-icon"><i className="fab fa-react"></i></span> }
+          { this.props.tech.includes('bootstrap') && <span className="tech-icon"><i className="fab fa-bootstrap"></i></span> }
+          {/* { this.props.tech.includes('css') && <span className="tech-icon"><i className="fab fa-css3"></i></span> }
+          { this.props.tech.includes('html') && <span className="tech-icon"><i className="fab fa-html5"></i></span> }  */}
+          { this.props.tech.includes('jquery') && <span className="tech-icon"><img style={styles} src='https://raw.githubusercontent.com/HappyMe777/portfolio/master/img/jquery-icon.png' alt=""/></span> }
           
         </div>
       );
